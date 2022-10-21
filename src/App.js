7c59fefc037bfc100b4aaf9f9cbbfd7d83876a14
AAli02 <aali.02p@gmail.com>
2022-10-21 10:27:58 -0400

    fixed, added books page with sorting

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import { books } from "./data";
 
 function App() {
   return (
-    <div className="App">
     <Router>
+      <div className="App">
+      <Nav />
       <Routes>
-        <Route path="/nav" element={<Nav />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/footer" element={<Footer />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/books" element={<Books books={books} />} />
       </Routes>
+      <Footer />
+    </div>
     </Router>
-        </div>
   );
 }
 
@@ -21,19 +24,3 @@ export default App;
 
 
 
-
-/*
-
-<Nav />
-<Route />
-<Home />
-<Footer /> 
-
-        <Route path="/nav" element={<Nav />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/footer" element={<Footer />} />
-
-      <div className="App">
-
-      </div>
-*/
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Discounted from "../components/Discounted";
 import Featured from "../components/Featured";
 import Highlights from "../components/Highlights";
 import Landing from "../components/Landing";
-import Explore from "../rfce";
+import Explore from "../components/Explore";
 
 const Home = () => {
   return (
